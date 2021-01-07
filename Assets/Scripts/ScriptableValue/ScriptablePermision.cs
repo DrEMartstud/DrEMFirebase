56174002923b86f56b0774b88f9f0274fa63ccd6
Andrey <DrEMartstud@gmail.com>
2021-01-07 20:04:59 +0200

    Firebase

---
+++ Assets/Scripts/ScriptableValue/ScriptablePermision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+
+    [CreateAssetMenu(fileName = "ScriptablePermisionValue")]
+    public class ScriptablePermisionValue : ScriptableObject {
+        
+        public string user;
+        public bool read;
+        public bool write;
+        public bool execute;
+
+    }
+}
