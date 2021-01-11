b5b6498408e068d2cdc1dfdba5150a09e1dedc1e
Andrey <DrEMartstud@gmail.com>
2021-01-11 22:44:37 +0200

    Reworked admin screen
    toggles instead of buttons
    added admin panel
    permisions update when admin selects different users in dropdown menu

--- Assets/Scripts/Game/Object.cs
+++ Assets/Scripts/Game/Object.cs
@@ -12,60 +12,69 @@ namespace UI {
         private Text _fileNameLabel;
         [SerializeField] 
         private Image _iconImage;
-        
-        [SerializeField] 
-        private Button _buttonRead;
-        
-        [SerializeField] 
-        private Button _buttonWrite;
+
+        [SerializeField]
+        private Toggle _readToggle;
         
         [SerializeField]
-        private Button _buttonExecute;
+        private Toggle _writeToggle;
+        
+        [SerializeField]
+        private Toggle _executeToggle;
 
+        private string _userName;
+        
         void Start() {
-            // _buttonRead.onClick.AddListener(Read);
-            // _buttonWrite.onClick.AddListener(Write);
-            // _buttonExecute.onClick.AddListener(Execute);
-                
+            _readToggle.onValueChanged.AddListener(delegate { HandleToggles(); });
+            _writeToggle.onValueChanged.AddListener(delegate { HandleToggles(); });
+            _executeToggle.onValueChanged.AddListener(delegate { HandleToggles(); });
         }
 
-        public void SetData(ScriptablePermisionValue permisionSO, string userLogin) {
+        public void SetData(ScriptablePermisionValue permisionSO, string userLogin, bool isAdmin) {
             _fileNameLabel.text = permisionSO.fileName;
             _iconImage.sprite = permisionSO.iconImage;
-            if (userLogin == "admin") {
-                SetButtons(true, true, true);
-                return;
-            }
+            _userName = userLogin;
+            
+            SetPerisionsPanel(true);
             foreach (var user in permisionSO.Users) {
                 if (userLogin == user.userName.ToLower()) {
-                    SetButtons(user.read, user.write, user.execute);
+                    SetPermisions(user.read, user.write, user.execute, isAdmin);
                     return;
-                } 
+                }
+                else {
+                    SetPermisions(false, false, false, isAdmin);
+                }
             }
-            gameObject.SetActive(false);
+            if (!isAdmin) {
+                gameObject.SetActive(false);
+            }
         }
 
         private void OnDisable() {
-            SetButtons(false, false, false);
             Destroy(gameObject);
         }
 
-        private void SetButtons(bool read, bool write, bool execute) {
-            _buttonRead.gameObject.SetActive(read);
-            _buttonWrite.gameObject.SetActive(write);
-            _buttonExecute.gameObject.SetActive(execute);
+        private void SetPerisionsPanel(bool state) {
+            _readToggle.gameObject.SetActive(state);
+            _writeToggle.gameObject.SetActive(state);
+            _executeToggle.gameObject.SetActive(state);
         }
 
-        // private void Read() {
-        //     
-        // }
-        //
-        // private void Write() {
-        //     
-        // }
-        //
-        // private void Execute() {
-        //     
-        // }
+        private void SetPermisions(bool read, bool write, bool execute, bool isAdmin) {
+            _readToggle.isOn = read;
+            _readToggle.interactable = isAdmin;
+            _writeToggle.isOn = write;
+            _writeToggle.interactable = isAdmin;
+            _executeToggle.isOn = execute;
+            _executeToggle.interactable = isAdmin;
+        }
+
+        private void HandleToggles() {
+            var r = _readToggle.isOn;
+            var w = _writeToggle.isOn;
+            var e = _executeToggle.isOn;
+            var f = _fileNameLabel.text;
+            Debug.Log(f);
+        }
     }
 }
--- Assets/Scripts/UI/AdminScreen.cs
+++ Assets/Scripts/UI/AdminScreen.cs
@@ -29,23 +29,56 @@ namespace UI {
         [SerializeField] 
         private Transform _containerForObjects;
         
+        [SerializeField]
+        private Dropdown _userOptions;
+        
         private void Start() {
             _exitButton.onClick.AddListener(Exit);
         }
         
         private void OnEnable() {
-            _welcomeMessageStringValue.text = $"Добро пожаловать, {_parsedLogin.value}!";
-            SpawnObjects();
+            _userOptions.onValueChanged.AddListener(delegate { SetScreen(); });
+            SetScreen();
         }
 
         private void OnDisable() {
             _parsedLogin.value = "";
         }
 
-        private void SpawnObjects() {
+        private void SetScreen() {
+            _containerForObjects.gameObject.SetActive(false);
+            _containerForObjects.gameObject.SetActive(true);
+            bool isAdmin = _parsedLogin.value == "admin";
+            _userOptions.gameObject.SetActive(isAdmin);
+            _welcomeMessageStringValue.text = $"Добро пожаловать, {_parsedLogin.value}!";
+            if (!isAdmin) {
+                SpawnObjects(_parsedLogin.value, isAdmin);
+                return;
+            }
+            else {
+                _userOptions.options.Clear();
+                for (int i = 0; i < SaveSystem.SavedDatas.Count; i++) {
+                    _userOptions.options.Add(new Dropdown.OptionData(){ text = SaveSystem.SavedDatas[i].login});
+                }
+                SpawnObjects(_userOptions.options[_userOptions.value].text, isAdmin);
+            }
+        }
+        
+        private void SpawnObjects(string userLogin, bool isAdmin) {
             foreach (var fileObject in _fileObjects) {
                 var objectView = Instantiate(_objectViewTemplate, _containerForObjects);
-                objectView.SetData(fileObject, _parsedLogin.value);
+                objectView.SetData(fileObject, userLogin, isAdmin);
+            }
+        }
+
+        public void SetPermisionsOnFile(bool r, bool w, bool e, string fileName, string userLogin) {
+            var foundFile = _fileObjects.Find(file => file.fileName == fileName);
+            foreach (var user in foundFile.Users) {
+                if (user.userName == userLogin) {
+                    user.read = r;
+                    user.write = w;
+                    user.execute = e;
+                }
             }
         }
         
